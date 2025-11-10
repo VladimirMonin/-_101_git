6f4c8342260b1c2e1c232c53fd1ea4ff42a26bc6
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-11-10 13:35:32 +0500

    Еще один коммит в ветку main

--- two.py
+++ two.py
@@ -1,2 +1,9 @@
 # Мой второй код будет тут
 print("Привет из второго файла!")
+
+# Как установить глобально и локально git config
+# git config --global user.name "Ваше Имя"
+# git config --global user.email "ваш.
+# Локально в репозитории
+# git config user.name "Локальное Имя"
+# git config user.email "локальный.емейл@example.com"
